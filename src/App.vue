5e4f34c6bec91222648432957cabe833df198ec5
hang <2283582431@qq.com>
2024-01-28 16:40:39 +0800

    add first loading

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const loadingBox = document.querySelector<HTMLDivElement>('.loading-box')
+  if (loadingBox) {
+    loadingBox.style.opacity = '0'
+    setTimeout(() => {
+      loadingBox.style.display = 'none'
+    }, 2000)
+  }
+})
+</script>
 
 <template>
   <router-view />

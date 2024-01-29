5c4f0e1cc2524eebb5039a37ff4f6d79f89cc8d6
hang <2283582431@qq.com>
2024-01-29 16:26:09 +0800

    add order-total

---
+++ src/views/main/dashbord/cpns/OrderTotal.vue
@@ -0,0 +1,192 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+interface OrdersTotalVO {
+  /**
+   * 订单数
+   */
+  nums?: number
+  /**
+   * 订单状态
+   */
+  status?: number
+}
+interface OrdersTotal extends OrdersTotalVO {
+  title?: string
+  color?: string
+  class?: string
+  icon?: string
+}
+
+enum OrdersStatus {
+  /** 待付款 */
+  UN_PAID = 0,
+  /** 已付款 */
+  PAID = 1,
+  /** 已发货 */
+  DELIVERED = 2,
+  /** 已收货 */
+  RECEIVED = 3,
+  /** 已评价 */
+  COMMENTED = 4,
+  /** 已取消 */
+  CANCELED = 5,
+  /** 已超时取消 */
+  DELAY_CANCELED = 6,
+  /** 发起退款 */
+  REFUND = 7,
+  /** 退款成功并取消 */
+  REFUND_SUCCESS = 8,
+}
+const orderTotalList = ref<OrdersTotal[]>([
+  {
+    nums: 0,
+    status: OrdersStatus.UN_PAID,
+    ...getTitle(OrdersStatus.UN_PAID),
+  },
+  {
+    nums: 0,
+    status: OrdersStatus.PAID,
+    ...getTitle(OrdersStatus.PAID),
+  },
+  {
+    nums: 0,
+    status: OrdersStatus.DELIVERED,
+    ...getTitle(OrdersStatus.DELIVERED),
+  },
+  {
+    nums: 0,
+    status: OrdersStatus.RECEIVED,
+    ...getTitle(OrdersStatus.RECEIVED),
+  },
+  {
+    nums: 0,
+    status: OrdersStatus.COMMENTED,
+    ...getTitle(OrdersStatus.COMMENTED),
+  },
+  {
+    nums: 0,
+    status: OrdersStatus.CANCELED,
+    ...getTitle(OrdersStatus.CANCELED),
+  },
+  {
+    nums: 0,
+    status: OrdersStatus.DELAY_CANCELED,
+    ...getTitle(OrdersStatus.DELAY_CANCELED),
+  },
+  {
+    nums: 0,
+    status: OrdersStatus.REFUND,
+    ...getTitle(OrdersStatus.REFUND),
+  },
+  {
+    nums: 0,
+    status: OrdersStatus.REFUND_SUCCESS,
+    ...getTitle(OrdersStatus.REFUND_SUCCESS),
+  },
+])
+
+async function loadData() {}
+loadData()
+
+function getTitle(status?: OrdersStatus) {
+  switch (status) {
+    case OrdersStatus.UN_PAID:
+      return {
+        title: '待付款',
+        color: '',
+      }
+    case OrdersStatus.PAID:
+      return {
+        title: '待发货',
+        color: '#fff',
+        class: 'relative text-#fff group bg-[var(--el-color-primary)] shadow ',
+        icon: 'animate-[1s_shopcart-rote] p-1.4em i-solar:cart-large-2-bold-duotone left-1 bottom-0  z-0 opacity-40 -rotate-20',
+      }
+    case OrdersStatus.DELIVERED:
+      return {
+        title: '已发货',
+        class: 'group',
+        icon: 'opacity-0 scale-50 transition-all -translate-x-20px group-hover:(opacity-30 translate-x-0 scale-100  -rotate-20)   p-1.4em i-solar:scooter-bold-duotone left-1 bottom-1 z-0  ',
+      }
+    case OrdersStatus.RECEIVED:
+      return {
+        title: '已收货',
+        color: '',
+      }
+    case OrdersStatus.COMMENTED:
+      return {
+        title: '已评价',
+        color: '#fff',
+        class: 'relative text-#fff shadow group bg-[var(--el-color-success)] ',
+        icon: 'animate-[1s_shopcart-rote] p-1.4em i-solar:bag-check-bold left-1 bottom-0  z-0 opacity-40 -rotate-20',
+      }
+    case OrdersStatus.CANCELED:
+      return {
+        title: '已取消',
+        class: 'group',
+        icon: 'opacity-0 scale-50 transition-all -translate-x-20px group-hover:(opacity-20 translate-x-0 scale-100  -rotate-20) p-1.4em i-solar:cart-cross-bold-duotone left-1 bottom-1 z-0  ',
+      }
+    case OrdersStatus.DELAY_CANCELED:
+      return {
+        title: '超时取消',
+        color: '',
+        class: 'group',
+        icon: 'opacity-0 scale-50 transition-all -translate-x-20px group-hover:(opacity-20 translate-x-0 scale-100  -rotate-20) p-1.4em i-solar:bag-cross-bold-duotone left-1 bottom-1 z-0  ',
+      }
+    case OrdersStatus.REFUND:
+      return {
+        title: '待退售后',
+        color: '#fff',
+        class: 'text-#fff bg-[var(--el-color-error)] opacity-90',
+        icon: 'animate-zoom-in-left p-1.4em i-solar:bill-check-bold-duotone left-1 bottom-1  z-0 opacity-40 -rotate-20',
+      }
+    case OrdersStatus.REFUND_SUCCESS:
+      return {
+        title: '已退款',
+        color: '',
+        class: 'group',
+        icon: 'opacity-0 scale-50 transition-all -translate-x-20px group-hover:(opacity-20 translate-x-0 -rotate-20 scale-100)   p-1.4em i-solar:banknote-2-broken left-1 bottom-1  z-0  ',
+      }
+    default:
+      return {
+        title: '待定',
+        color: '',
+      }
+  }
+}
+
+const router = useRouter()
+function gotoDetailOrder(p: OrdersTotal) {
+  router.push({
+    name: 'order',
+    query: {
+      status: p.status,
+    },
+  })
+}
+</script>
+
+<template>
+  <div>
+    <el-scrollbar>
+      <div class="flex select-none justify-between py-2px" min-h-5rem>
+        <!-- List -->
+        <div
+          v-for="p in orderTotalList"
+          :key="p.status"
+          class="relative mx-1 w-8rem flex-row-c-c flex-shrink-0 flex-col cursor-pointer overflow-hidden rounded-10px transition-200 lg:(mx-2 flex-1 p-2) hover-(shadow)"
+          :class="p.class"
+          @click="gotoDetailOrder(p)">
+          <span v-incre-up.int="p.nums" class="dark:text-light" :style="{ color: p.color }" my-4 text-1.4rem font-500>{{
+            p.nums
+          }}</span>
+          <small truncate pb-2 text-0.8rem font-600> {{ p.title }}</small>
+          <i v-if="p.icon" absolute :class="p.icon" />
+        </div>
+      </div>
+    </el-scrollbar>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/main/dashbord/index.vue
+++ src/views/main/dashbord/index.vue
@@ -1,10 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import OrderTotal from './cpns/OrderTotal.vue'
+</script>
 
 <template>
   <div class="flex flex-col tracking-0.1em">
     <WelcomeCard />
-    123
+    <div grid="~ cols-1 gap-8 items-start" lg:grid="~ cols-[4fr_2fr]  gap-8 " mb-4 class="content">
+      <div class="left" grid="~ cols-1 gap-8">
+        <!-- 订单数 -->
+        <OrderTotal class="box-content" />
+        <div>123</div>
+        <div>123</div>
+        <div>123</div>
+      </div>
+      <div class="right" grid="~ cols-1 gap-8">
+        <div>123</div>
+        <div>123</div>
+        <div>123</div>
+        <div>123</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.box-content {
+  --at-apply: 'v-card p-4  md:p-6 bg-white dark:bg-dark-5 rounded-3 shadow-sm';
+  box-shadow:
+    rgba(133, 133, 133, 0.1) 0px 1px 3px,
+    rgba(128, 128, 128, 0.1) 0px 1px 2px;
+}
+.left :deep(.el-scrollbar__thumb) {
+  display: none;
+}
+</style>
